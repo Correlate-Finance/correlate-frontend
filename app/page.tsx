e3416fe037deec1bf61d6fc5328230d48bcf366c
Shashank Goyal <shashankgoyal095@gmail.com>
2024-02-08 14:33:57 -0500

    fix lag period selector

--- app/page.tsx
+++ app/page.tsx
@@ -369,7 +369,7 @@ const Page = () => {
                     <SelectItem value="1">1</SelectItem>
                     <SelectItem value="2">2</SelectItem>
                     <SelectItem value="3">3</SelectItem>
-                    <SelectItem value="3">4</SelectItem>
+                    <SelectItem value="4">4</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
