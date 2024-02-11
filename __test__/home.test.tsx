13cdf037132a74f9ffe44137d72b5f77ca23ced3
Asrul Kadir <asrulkadir.asus4@gmail.com>
2024-02-12 00:57:48 +0800

    initial setup unit test next js

---
+++ __test__/home.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import Page from '../app/page';
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />);
+
+    const heading = screen.getAllByText('Correlate');
+
+    expect(heading).toBeDefined();
+  });
+});
