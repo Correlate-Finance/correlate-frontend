7549066e1eb6592394a0eeaf9c23c5a998e6cee9
Asrul Kadir <asrulkadir.asus4@gmail.com>
2024-02-11 16:02:32 +0800

    feat: export data to excel

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import * as XLSX from 'xlsx';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -21,3 +22,41 @@ export function convertToExcel(data: number[], date: string[]) {
     data.map((dataItem, index) => `${date[index]}\t${dataItem}`).join('\n')
   );
 }
+
+export const exportToExcel = (data: any) => {
+  if (!data?.length) {
+    alert('No data to export');
+    return;
+  }
+  const dataObjValues = data?.map((item: any) => {
+    const objValues = Object.values(item);
+    return objValues;
+  }) as (string | number)[][];
+  const dataObjKeys = Object.keys(data?.[0] || {});
+  const excelData = [dataObjKeys, ...dataObjValues];
+
+  const ws = XLSX.utils.aoa_to_sheet(excelData);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+  const blob = XLSX.write(wb, {
+    bookType: 'xlsx',
+    type: 'array',
+  });
+
+  const newBlob = new Blob([blob], {
+    type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  });
+
+  const url = URL.createObjectURL(newBlob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'data.xlsx';
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
