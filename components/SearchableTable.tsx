335b036b21113ea434143f0d030311e24777b880
Shashank Goyal <shashankgoyal095@gmail.com>
2024-04-22 12:38:34 -0400

    Don't sort clicks on checkedRowsChange

--- components/SearchableTable.tsx
+++ components/SearchableTable.tsx
@@ -66,6 +66,7 @@ export default function SearchableTable({
     setCheckedRows(newCheckedRows);
   };
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const filteredData = useMemo(
     () =>
       dropdownFilterData
